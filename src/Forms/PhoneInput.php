b92e0053d14be1b3413ceb62dcd9deb2ea964386
Benjamin Locher <blocher@gmail.com>
2025-06-25 22:08:00 -0400

    upgrading to v4 filament

--- src/Forms/PhoneInput.php
+++ src/Forms/PhoneInput.php
@@ -2,11 +2,11 @@
 
 namespace Ysfkaya\FilamentPhoneInput\Forms;
 
+use Filament\Schemas\Components\Contracts\HasAffixActions;
 use Closure;
 use Filament\Forms\Components\Concerns\HasAffixes;
 use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
-use Filament\Forms\Components\Contracts\HasAffixActions;
 use Filament\Forms\Components\Field;
 use Filament\Pages\Page;
 use Filament\Support\RawJs;
